95053951a373d72664b3aaa1cca36be4739c4717
azizbek Xabibullayev <115640101+Azizbek0606@users.noreply.github.com>
2024-05-22 00:16:12 +0500

    update

--- scripts/app.js
+++ scripts/app.js
@@ -31,8 +31,6 @@ let banner_subtitle = document.querySelector(".banner_subtitle h2");
 let bg_image_index = 0;
 let swip_anime = document.querySelector('.swip_anime');
 
-let imagesLoaded = 0; // global variable to track the number of images loaded
-
 // Function to preload images
 function preloadImages(urls, callback) {
     let loadedImages = 0;
@@ -54,6 +52,7 @@ function initBanner() {
     // Call preloadImages function before setting interval
     preloadImages(Object.values(data_banner).map(item => item.url), function () {
         setInterval(set_image_to_banner, 5000);
+        set_image_to_banner(); // Show the first banner immediately
     });
 }
 
@@ -65,17 +64,17 @@ function set_image_to_banner() {
     let keys = Object.keys(data_banner);
     let key = keys[bg_image_index];
     let data_len = Object.keys(data_banner).length;
-    banner_wrapper.style.backgroundImage = `url(${data_banner[key].url})`;
+    banner_wrapper.style.backgroundImage = `url(${ data_banner[key].url })`;
     banner_title.innerText = data_banner[key].title;
     banner_subtitle.innerText = data_banner[key].description;
 
     swip_anime.style.backgroundColor = data_banner[key].swip_color;
     if (swip_anime.dataset.state === "right") {
-        swip_anime.style.transform = `translateX(-100%)`;
+        swip_anime.style.transform = "translateX(-100%)";
         swip_anime.dataset.state = "left";
         swip_anime.style.clipPath = "polygon(100% 0%, 75% 50%, 100% 100%, 25% 100%, 0% 50%, 25% 0%)";
     } else {
-        swip_anime.style.transform = `translateX(100%)`;
+        swip_anime.style.transform = "translateX(100%)";
         swip_anime.style.clipPath = "polygon(75% 0%, 100% 50%, 75% 100%, 0% 100%, 25% 50%, 0% 0%)";
         swip_anime.dataset.state = "right";
     }
@@ -84,10 +83,6 @@ function set_image_to_banner() {
 }
 
 swip_anime.dataset.state = "right";
-
-set_image_to_banner();
-
-
 function add_class_name(class_name , element){
     element.classList.toggle(class_name);
 }
