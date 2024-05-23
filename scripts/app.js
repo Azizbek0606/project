e497f501a5f1e6d565ac1e7de1872264d6d75782
azizbek Xabibullayev <115640101+Azizbek0606@users.noreply.github.com>
2024-05-23 16:30:07 +0500

    update

--- scripts/app.js
+++ scripts/app.js
@@ -1,30 +1,5 @@
-let data_banner = {
-    "dubai": {
-        "title": "Dubai",
-        "description": "Assalomu alakum biz bilan dubayga arzon narxlarda maroqli sayoxat qilishni istaysizmi unda darxol bizga murojat qiling",
-        "url": "../images/banner_images/dubai.jpg",
-        "swip_color": '#E6E6FA70'
-    },
-    "misr": {
-        "title": "Misr",
-        "description": "Assalomu alakum Misrga bilan dubayga arzon narxlarda maroqli sayoxat qilishni istaysizmi unda darxol bizga murojat qiling",
-        "url": "../images/banner_images/misr.jpg",
-        "swip_color": '#ef6c0070'
-    },
-    "antaliya": {
-        "title": "Antaliya",
-        "description": "Assalomu alakum biz bilan Antaliyaga arzon narxlarda maroqli sayoxat qilishni istaysizmi unda darxol bizga murojat qiling",
-        "url": "../images/banner_images/antaliya.jpg",
-        "swip_color": '#0F2C5470'
-    },
-    "balgariya": {
-        "title": "Bolgariya",
-        "description": "Assalomu alakum biz bilan Bolgariyaga arzon narxlarda maroqli sayoxat qilishni istaysizmi unda darxol bizga murojat qiling",
-        "url": "../images/banner_images/balgariya.jpg",
-        "swip_color": '#00808070'
-    }
-};
-
+import { data_banner } from './data.js';
+import { about_card_content } from './data.js';
 let banner_wrapper = document.querySelector(".banner_wrapper");
 let banner_title = document.querySelector(".banner_title h1");
 let banner_subtitle = document.querySelector(".banner_subtitle h2");
@@ -47,12 +22,11 @@ function preloadImages(urls, callback) {
     }
 }
 
-// Initialize banner after preloading images
 function initBanner() {
     const urls = Object.values(data_banner).map(item => item.url);
     preloadImages(urls, function () {
         setInterval(set_image_to_banner, 5000);
-        set_image_to_banner(); // Show the first banner immediately
+        set_image_to_banner();
     });
 }
 
@@ -60,14 +34,8 @@ function set_image_to_banner() {
     const keys = Object.keys(data_banner);
     const key = keys[bg_image_index];
     const data = data_banner[key];
-
-    // Create a new image element to ensure it's loaded before setting it as background
     const img = new Image();
     img.onload = function () {
-        banner_wrapper.style.backgroundImage = `url(${data.url})`;
-        banner_title.innerText = data.title;
-        banner_subtitle.innerText = data.description;
-
         swip_anime.style.backgroundColor = data.swip_color;
         if (swip_anime.dataset.state === "right") {
             swip_anime.style.transform = "translateX(-100%)";
@@ -78,15 +46,63 @@ function set_image_to_banner() {
             swip_anime.style.clipPath = "polygon(75% 0%, 100% 50%, 75% 100%, 0% 100%, 25% 50%, 0% 0%)";
             swip_anime.dataset.state = "right";
         }
-
+        banner_wrapper.style.backgroundImage = `url(${data.url})`;
+        banner_title.innerText = data.title;
+        banner_subtitle.innerText = data.description;
         bg_image_index = (bg_image_index + 1) % keys.length;
     };
-    img.src = data.url; // Trigger the load event
+    img.src = data.url;
 }
 
 initBanner();
 
 swip_anime.dataset.state = "right";
+const cardWrapper = document.querySelector('.about_card_wrapper');
+function createCard(data, parentElement) {
+    const fragment = document.createDocumentFragment();
+    let delay = 0;
+    Object.keys(data).forEach(key => {
+        const { title, description, url } = data[key];
+
+        const card = document.createElement('div');
+        card.className = 'card';
+        card.setAttribute("data-aos", "fade-up");
+        card.setAttribute("data-aos-delay", delay);
+
+        const cardImgWrapper = document.createElement('div');
+        cardImgWrapper.className = 'card_image';
+
+        const cardImg = document.createElement('img');
+        cardImg.src = url;
+        cardImgWrapper.appendChild(cardImg);
+
+        const cardTitle = document.createElement('div');
+        cardTitle.className = 'card_title';
+
+        const cardTitleH2 = document.createElement('h2');
+        cardTitleH2.innerText = title;
+        cardTitle.appendChild(cardTitleH2);
+
+        const cardContent = document.createElement('div');
+        cardContent.className = 'card_content';
+
+        const cardContentP = document.createElement('p');
+        cardContentP.innerText = description;
+        cardContent.appendChild(cardContentP);
+
+        card.appendChild(cardImgWrapper);
+        card.appendChild(cardTitle);
+        card.appendChild(cardContent);
+
+        fragment.appendChild(card);
+        delay += 100;
+    });
+    parentElement.appendChild(fragment);
+}
+
+createCard(about_card_content, cardWrapper);
+
+
 function add_class_name(class_name , element){
     element.classList.toggle(class_name);
 }
---
+++ scripts/data.js
@@ -0,0 +1,48 @@
+export let data_banner = {
+    "dubai": {
+        "title": "Dubai",
+        "description": "Assalomu alakum biz bilan dubayga arzon narxlarda maroqli sayoxat qilishni istaysizmi unda darxol bizga murojat qiling",
+        "url": "../images/banner_images/dubai.jpg",
+        "swip_color": '#E6E6FA70'
+    },
+    "misr": {
+        "title": "Misr",
+        "description": "Assalomu alakum Misrga bilan dubayga arzon narxlarda maroqli sayoxat qilishni istaysizmi unda darxol bizga murojat qiling",
+        "url": "../images/banner_images/misr.jpg",
+        "swip_color": '#ef6c0070'
+    },
+    "antaliya": {
+        "title": "Antaliya",
+        "description": "Assalomu alakum biz bilan Antaliyaga arzon narxlarda maroqli sayoxat qilishni istaysizmi unda darxol bizga murojat qiling",
+        "url": "../images/banner_images/antaliya.jpg",
+        "swip_color": '#0F2C5470'
+    },
+    "balgariya": {
+        "title": "Bolgariya",
+        "description": "Assalomu alakum biz bilan Bolgariyaga arzon narxlarda maroqli sayoxat qilishni istaysizmi unda darxol bizga murojat qiling",
+        "url": "../images/banner_images/balgariya.jpg",
+        "swip_color": '#00808070'
+    }
+};
+export let about_card_content = {
+    'protect':{
+        'title' : "Ishonchli",
+        'description': "Qaqnus tour  bilan dunyoning minglab ajoyib shaharlarini ishonchli va xavfsiz tarzda kashf eting!",
+        'url' : "../images/card/protect.png"
+    },
+    'quality': {
+        'title': "Sifatli",
+        'description': "Qaqnus tour jamoasi orqali sifatli sayohatga otlaning!",
+        'url': "../images/card/quality.png"
+    },
+    'prices': {
+        'title': "Hamyonbob",
+        'description': "Qaqnus Tour bilan arzon va sifatli sayohatlarga chiqing!",
+        'url': "../images/card/prices.png"
+    },
+    'git': {
+        'title': "Git ko'magi",
+        'description': "Qaqnus Tour va git bilan ajoyib shaharlarni yanada chuqurroq o'rganing!",
+        'url': "../images/card/git.png"
+    },
+};
