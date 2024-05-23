dff472c411f2453e7b87398127d61296c6598b92
azizbek Xabibullayev <115640101+Azizbek0606@users.noreply.github.com>
2024-05-23 21:05:34 +0500

    update

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,15 @@
 import { about_card_content } from './data.js';
 import { data_banner } from './data.js';
 import { prices_card_data } from './data.js';
+
 let banner_wrapper = document.querySelector(".banner_wrapper");
 let banner_title = document.querySelector(".banner_title");
 let banner_subtitle = document.querySelector(".banner_subtitle");
 let bg_image_index = 0;
 let swip_anime = document.querySelector('.swip_anime');
-
 let menu_toggler = document.querySelectorAll(".menu_toggler");
 let nav_context = document.querySelector('#nav_context');
+
 for (let i = 0; i < menu_toggler.length; i++) {
     menu_toggler[i].addEventListener("click", ()=>{
         nav_context.classList.toggle("active_nav_context");
@@ -81,52 +82,6 @@ function set_image_to_banner() {
 initBanner();
 
 swip_anime.dataset.state = "right";
-// function createCard(data, parentElement) {
-//     const fragment = document.createDocumentFragment();
-//     let delay = 0;
-//     Object.keys(data).forEach(key => {
-//         const { title, description, url } = data[key];
-
-//         const card = document.createElement('div');
-//         card.className = 'card';
-//         card.setAttribute("data-aos", "fade-up");
-//         card.setAttribute("data-aos-delay", delay);
-
-//         const cardImgWrapper = document.createElement('div');
-//         cardImgWrapper.className = 'card_image';
-
-//         const cardImg = document.createElement('img');
-//         cardImg.src = url;
-//         cardImgWrapper.appendChild(cardImg);
-
-//         const cardTitle = document.createElement('div');
-//         cardTitle.className = 'card_title';
-
-//         const cardTitleH2 = document.createElement('h2');
-//         cardTitleH2.innerText = title;
-//         cardTitle.appendChild(cardTitleH2);
-
-//         const cardContent = document.createElement('div');
-//         cardContent.className = 'card_content';
-
-//         const cardContentP = document.createElement('p');
-//         cardContentP.innerText = description;
-//         cardContent.appendChild(cardContentP);
-
-//         card.appendChild(cardImgWrapper);
-//         card.appendChild(cardTitle);
-//         card.appendChild(cardContent);
-
-//         fragment.appendChild(card);
-//         delay += 100;
-//     });
-//     parentElement.appendChild(fragment);
-// }
-
-// createCard(about_card_content, cardWrapper);
-
-
-
 function createCard(data, parentElement, type = 'default') {
     const fragment = document.createDocumentFragment();
     let delay = 0;
@@ -138,7 +93,7 @@ function createCard(data, parentElement, type = 'default') {
         if (type === 'default') {
             card = createDefaultCard(title, description, url, delay);
         } else if (type === 'prices') {
-            card = createPricesCard(title, description, url);
+            card = createPricesCard(title, description, url , delay);
         }
 
         fragment.appendChild(card);
@@ -161,9 +116,11 @@ function createDefaultCard(title, description, url, delay) {
     return card;
 }
 
-function createPricesCard(title, description, url) {
+function createPricesCard(title, description, url , delay) {
     const pricesCardWrapper = document.createElement('div');
     pricesCardWrapper.className = 'prices_card';
+    pricesCardWrapper.setAttribute('data-aos', 'fade-up');
+    pricesCardWrapper.setAttribute('data-aos-delay', delay);
     let img = createElement('div', 'prices_image', createImage(url));
     pricesCardWrapper.appendChild(img);
     const pricesCard = createElement('div', 'prices_card_content_block',
